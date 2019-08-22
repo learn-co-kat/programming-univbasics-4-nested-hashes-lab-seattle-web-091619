fc5ab3f0c43b7d65a7d37c028e68181a26b6b198
Katrina R <katrina.reinsdorff@gmail.com>
2019-08-22 22:22:45 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -76,7 +76,6 @@ def adding_matz
      }
         
   programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby",:languages => ["LISP", "C"]}
-  
   return programmer_hash 
 end
 
@@ -100,7 +99,7 @@ def changing_alan
      alans_new_info = "GUI"
      
      programmer_hash[:alan_kay][:known_for] = alans_new_info
-     
+     return programmer_hash 
 end
 
 def adding_to_dennis
